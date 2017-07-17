f85b2092ec6ff0525fae9e3487ac2484bae0f7a6
Erik Mossberg <lingonvecka@gmail.com>
2017-07-17 19:08:20 +0200

    remove unused constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,3 @@
 export const GAS_COST = 35000;
 export const TIME_BETWEEN_UPDATES = 2 * 60 * 60 * 1000;
-export const SMOOTHING_FACTOR = 0.95;
 export const POLLING_DELAY = 6000;
-
-// number of values to collect before sending first update
-export const CONVERGENCE_THRESHOLD = 100;
